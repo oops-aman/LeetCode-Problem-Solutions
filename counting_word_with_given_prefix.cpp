9b37fac82bed802b724e93f0722b90babcb284f1
oops-aman <b200054@nitsikkim.ac.in>
2022-06-06 09:27:33 +0530

    Counting Words with a given Prefix

---
+++ counting_word_with_given_prefix.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int prefixCount(vector<string>& words, string pref) {
+        int n = pref.size();
+        int count = 0;
+        for(auto x: words) {
+            string sub = x.substr(0, n);
+            if(sub == pref)
+                count++;
+        }
+        return count;
+    }
+};
