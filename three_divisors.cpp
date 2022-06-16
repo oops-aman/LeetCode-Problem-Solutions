2c968887afe46be1da7073ed00786b4b7d18d179
oops-aman <b200054@nitsikkim.ac.in>
2022-06-16 10:59:57 +0530

    Three Divisors

---
+++ three_divisors.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isPrime(int n) {
+        if(n <= 1)
+            return false;
+        for(int i = 2; i <= sqrt(n); i++) {
+            if(n % i == 0)
+                return false;
+        }
+        return true;
+    }
+    bool isThree(int n) {
+        int num = sqrt(n);
+        return ((num * num == n) and isPrime(num));
+    }
+};
