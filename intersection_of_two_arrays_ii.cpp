1c32900490d4ac110cba3cbf085247620d039190
oops-aman <b200054@nitsikkim.ac.in>
2022-06-19 10:59:52 +0530

    Intersection of Two Arrays II

---
+++ intersection_of_two_arrays_ii.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        vector<int> result;
+        
+        sort(nums1.begin(), nums1.end());
+        sort(nums2.begin(), nums2.end());
+        
+        int i = 0, j = 0;
+        int m = nums1.size(), n = nums2.size();
+        
+        while(i < m and j < n) {
+            if(nums1[i] < nums2[j]) {
+                i++;
+            } else if(nums1[i] > nums2[j]) {
+                j++;
+            } else {
+                result.push_back(nums1[i]);
+                i++, j++;
+            }
+        }
+        return result;
+    }
+};
