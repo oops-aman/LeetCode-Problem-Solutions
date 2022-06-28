40d01f0227085e7adf67e54591c1728acb4fccbd
oops-aman <b200054@nitsikkim.ac.in>
2022-06-28 11:56:06 +0530

    Simplified Fractions

---
+++ simplified_fractions.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<string> simplifiedFractions(int n) {
+        vector<string> ans;
+        if(n == 2) 
+            return {"1/2"};
+        
+        for(int i = 2; i <= n; i++) {
+            for(int j = 1; j < i; j++) {
+                if(gcd(i, j) == 1) {
+                    string s = to_string(j) + "/" + to_string(i);
+                    ans.push_back(s);
+                }
+            }
+        }
+        return ans;
+    }
+};
