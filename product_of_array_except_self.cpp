a2c3eaa52439966537d21c7c9bcceb6c9f0570e2
Aman Saurav <83208906+oops-aman@users.noreply.github.com>
2022-07-01 07:31:32 +0530

    Product of Array Except Self

--- product_of_array_except_self.cpp
+++ product_of_array_except_self.cpp
@@ -11,7 +11,6 @@ public:
         for(int i = 1; i < n; i++) {
             prefix[i] = nums[i-1] * prefix[i-1];
         }
-        
         suffix[n-1]=1;
         for(int i = n-2; i >= 0; i--) {
             suffix[i] = nums[i+1] * suffix[i+1];
