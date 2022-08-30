1d56f4f93258fae1bc0fca2f48496122fe767c51
oops-aman <b200054@nitsikkim.ac.in>
2022-08-30 23:59:50 +0530

    Implement strStr()

---
+++ implement_strStr().cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        int n = haystack.length(), m = needle.length();
+        if(m > n) return -1;
+        if(haystack == needle) return 0;
+        for(int i = 0; i < n; i++) {
+            int j;
+            for(j = 0; j < m; j++) {
+                if(haystack[i +  j] != needle[j])
+                    break;
+            }
+            if(j == m)
+                return i;
+        }
+        return -1;
+    }
+};
